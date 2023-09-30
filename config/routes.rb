d1a933835804a5f167a26cf93c51ae916f010892
Hamanpreet <haman42626@gmail.com>
2023-09-30 18:38:18 -0400

    Feature/User Authentication

--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,16 @@ Rails.application.routes.draw do
     resources :categories, only: [:index, :new, :create]
   end
 
+ 
+
+  get '/signup' => 'users#new'
+  post '/signup' => 'users#create'
+  
+
+  get '/login' => 'sessions#new'
+  post '/login' => 'sessions#create'
+  post '/logout' => 'sessions#destroy'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

eed191dcfd4dd47dbf15767aa3d581d53ab6396f
Hamanpreet <haman42626@gmail.com>
2023-10-01 15:32:00 -0400

    Users are able to see active sales on the Home Page

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,5 +1,11 @@
 class Sale < ApplicationRecord
 
+  # AR scope(class method)
+  def self.active
+    where("sales.starts_on < ? AND sales.ends_on >= ?", 
+    Date.current, Date.current)
+  end
+
   def finished?
     self.ends_on < Date.current
   end

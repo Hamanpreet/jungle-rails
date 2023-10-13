a12a0940ec8706f59954f5cb8c986ef0aa04c98d
Hamanpreet <haman42626@gmail.com>
2023-10-13 14:40:06 -0400

    Add basic cypress test for Home page

---
+++ cypress/integration/home.spec.js
@@ -0,0 +1,10 @@
+describe('example to-do app', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it("There is products on the page", () => {
+    cy.get(".products article").should("be.visible");
+  });
+ 
+})

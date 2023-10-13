ab335681199d7050f9c361d206ba97614d34698b
Hamanpreet <haman42626@gmail.com>
2023-10-13 16:01:52 -0400

    Add tests for homepage of app

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,10 +1,10 @@
 describe('example to-do app', () => {
   beforeEach(() => {
-    cy.visit('http://localhost:3000/')
+    cy.visit('/')
   })
 
   it("There is products on the page", () => {
-    cy.get(".products article").should("be.visible");
+    cy.get(".products article").should("have.length", 2);
   });
  
 })
